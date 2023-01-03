922f54152c6a26265fcd345911dcabf30b2b25a3
Chris OBeirne <chris.obeirne@onyxdigital.com.au>
2023-01-03 21:54:06 +1100

    build: init commit

---
+++ SmartBuildingConsoleApp/DigitalTwins/DigitalTwinsManager.cs
@@ -0,0 +1,20 @@
+﻿using Azure.DigitalTwins.Core;
+using Azure.Identity;
+using System;
+using System.IO;
+namespace SmartBuildingConsoleApp.DigitalTwins
+{ 
+    public class DigitalTwinsManager 
+    { 
+        private static readonly string adtInstanceUrl = "<Azure Digital Twins Instance URL>"; 
+
+        private DigitalTwinsClient client; 
+
+        public DigitalTwinsManager() { Connect(); } 
+
+        public void Connect() 
+        { 
+        
+        } 
+    } 
+}
